33588fb8d3fc61978072e6da45ff56b870199692
Nochovnyi <nochovnyi_i@mail.ru>
2022-05-26 20:28:45 +0300

    objects homework v1

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,45 @@
 function Student(name, gender, age) {
-    // Ваш код
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
 
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
+  
+}
+
+Student.prototype.addMark = function (mark){
+  if(this.marks === undefined){ 
+    let arr = []
+    arr.push(mark)
+    this.marks = arr;
+    } else {
+      let arr = Object.values(this.marks)
+      arr.push(mark)
+      this.marks = arr
+    }
+}
+
+Student.prototype.addMarks = function(...marks){
+  this.marks = marks; 
+}
+
+Student.prototype.getAverage = function(){
+
+let sum = this.marks.reduce(function(acc, rate){
+  return acc + rate;
+});
+this.getAverage = sum / this.marks.length;
+return this.getAverage
+}
+
+
+Student.prototype.exclude = function(reason){
+  delete this.subjectName;
+  this.marks = null;
+  this.excluded = reason;
 }
 
 // ваш код для остальных методов
